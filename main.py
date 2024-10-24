6f97efbcbf4249c8d1052f2b4e6c4a31aec8ef36
fush0528 <167970556+fush0528@users.noreply.github.com>
2024-10-24 16:13:59 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    print: float
+    description : str = None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/items/")
+def create_item(item: Item):
+    print(f"Received item: {item}")
+    return {"message": "Item received", "item": item}
